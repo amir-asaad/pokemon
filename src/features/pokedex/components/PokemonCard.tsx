c335a58ffabdd4f69272daaa33596e1ef8456da4
amir-asaad <74415434+amir-asaad@users.noreply.github.com>
2022-12-08 14:21:40 +0800

    Chore: Use href on pokemon card click

--- src/features/pokedex/components/PokemonCard.tsx
+++ src/features/pokedex/components/PokemonCard.tsx
@@ -9,9 +9,6 @@ import {
 import React, { memo } from 'react';
 import { PokemonTypesInterface } from '../store/pokemonSlice';
 import { pokemonTypeColor } from '../../../utils/helpers';
-import { useNavigate } from 'react-router-dom';
-import { SET_VIEW_POKEMON } from '../store/pokemonSlice';
-import { useAppDispatch, useAppSelector } from '../../../hooks';
 interface Props {
   name: string;
   image: string;
@@ -19,10 +16,6 @@ interface Props {
 }
 
 const PokemonCard: React.FC<Props> = ({ name, image, types }) => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const { resultsData } = useAppSelector((state) => state.pokemon);
-
   const mapTypes = () => {
     return types.map(({ type }) => {
       const typeObject = pokemonTypeColor().find(
@@ -42,17 +35,6 @@ const PokemonCard: React.FC<Props> = ({ name, image, types }) => {
     });
   };
 
-  const onViewPokemon = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    event.stopPropagation();
-    const found = resultsData.find((val) => val.name === name);
-
-    dispatch(SET_VIEW_POKEMON(found));
-
-    navigate(`/${name}`);
-  };
-
   return (
     <Card
       raised
@@ -61,7 +43,7 @@ const PokemonCard: React.FC<Props> = ({ name, image, types }) => {
         mx: 'auto'
       }}
     >
-      <CardActionArea onClick={(event) => onViewPokemon(event)}>
+      <CardActionArea href={`/${name}`}>
         <CardMedia
           component="img"
           image={image}
